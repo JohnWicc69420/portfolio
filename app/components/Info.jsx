f6ee092028cf2af19d11d116786ccbc243bff0f2
JohnWicc69420 <tusmantariq9111@gmail.com>
2024-02-11 16:24:57 +0500

    needs data, design is working

---
+++ app/components/Info.jsx
@@ -0,0 +1,14 @@
+export default function Info() {
+  return (
+    <>
+      <div className="flex gap-3 flex-col items-start">
+        <div className="text-[#E2E8F0] text-5xl font-bold">Usman Tariq</div>
+        <div className="text-[#E2E8F0] text-xl">Front-End Web Developer</div>
+        <div className="">
+          I engineer dynamic and responsive user interfaces, bringing designs to
+          life with precision and flair.
+        </div>
+      </div>
+    </>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,22 @@
+import Info from "./components/Info";
+import About from "./components/About";
+
 export default function Home() {
   return (
-    <div className=" bg-[#0f172a] h-screen w-full text-[#5ADFCB] font-bold text-3xl p-10">
-      U
-    </div>
+    <>
+      <div
+        className="bg-[#0f172a] text-[#94A3B8] flex pt-16 items-start 
+      justify-center gap-20"
+      >
+        <div className="sticky top-16 w-[450px] p-10">
+          <Info />
+        </div>
+        <div className=" w-[600px]">
+          <div>
+            <About />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
